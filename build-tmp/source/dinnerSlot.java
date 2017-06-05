70bdb5263ff78e49737a6b9daf44ebb9ecd02375
ryu-g <b1013178@fun.ac.jp>
2017-06-05 19:24:15 +0900

    add secret command

--- build-tmp/source/dinnerSlot.java
+++ build-tmp/source/dinnerSlot.java
@@ -19,14 +19,19 @@ public class dinnerSlot extends PApplet {
 
 
 ArrayList<String> pool;
+ArrayList<String> cook;
 int i=0;
 int roll=-1;
+int mode=0;
+int command=0;
 String pop = "press space";
 
 public void setup() {
 	pool = new ArrayList<String>();
+	cook = new ArrayList<String>();
 	
 	setPool();
+	setCook();
 }
 
 
@@ -43,11 +48,25 @@ public void setPool(){
 	pool.add("Japanese SUSHI");
 }
 
+public void setCook(){
+	cook.add("omu-rice");
+	cook.add("gyu-dong");
+	cook.add("tomato soup");
+	cook.add("kara-age");
+	cook.add("NABE(kimuchi)");
+	cook.add("NABE(syouyu)");
+	cook.add("NABE(MISO)");
+	cook.add("hanburg");
+	cook.add("cha-han");
+	cook.add("katsu-dong");
+}
 public void draw() {
-	background(128);
 	textAlign(CENTER);
 	stroke(0);
 	textSize(25);
+if(mode==0){
+	background(128);
+
 	text("hold SPASE key", width/2, height/3);
 	text("today's dinner is...", width/2, 3*height/7);
 	if(keyPressed && key==' '){
@@ -59,9 +78,28 @@ public void draw() {
 	text(pop, width/2, 3*height/4);
 	if(roll<0)text("one more?", width/2, 5*height/6);
 }
+if(mode==1){
+	background(128,128,250);
+
+	text("hold SPASE key", width/2, height/3);
+	text("today's dinner is...", width/2, 3*height/7);
+	if(keyPressed && key==' '){
+		i=PApplet.parseInt(random(0,cook.size()-1));
+		pop =  cook.get(i);
+		println(pop);
+	}else if(keyPressed && key!=' '){
+	text("that is "+key+" key", width/2, 5*height/6);}
+	text(pop, width/2, 3*height/4);
+	if(roll<0)text("one more?", width/2, 5*height/6);
+}
+}
 
 public void keyPressed() {// while spase key pressed rikk slot
 	roll=1; //rolling now
+	if(command==0&&key=='c')command+=1;else command=0;
+	if(command==1&&key=='o')command+=1;else command=0;
+	if(command==2&&key=='0')command+=1;else command=0;
+	if(command==0&&key=='k'){println("mode: "+mode);mode=1;command=0;}else command=0;
 }
 
 public void keyReleased(){
--- dinnerslot.pde
+++ dinnerslot.pde
@@ -1,14 +1,19 @@
 import java.util.*;
 
 ArrayList<String> pool;
+ArrayList<String> cook;
 int i=0;
 int roll=-1;
+int mode=0;
+int command=0;
 String pop = "press space";
 
 void setup() {
 	pool = new ArrayList<String>();
+	cook = new ArrayList<String>();
 	size(400,300);
 	setPool();
+	setCook();
 }
 
 
@@ -25,11 +30,25 @@ void setPool(){
 	pool.add("Japanese SUSHI");
 }
 
+void setCook(){
+	cook.add("omu-rice");
+	cook.add("gyu-dong");
+	cook.add("tomato soup");
+	cook.add("kara-age");
+	cook.add("NABE(kimuchi)");
+	cook.add("NABE(syouyu)");
+	cook.add("NABE(MISO)");
+	cook.add("hanburg");
+	cook.add("cha-han");
+	cook.add("katsu-dong");
+}
 void draw() {
-	background(128);
 	textAlign(CENTER);
 	stroke(0);
 	textSize(25);
+if(mode==0){
+	background(128);
+
 	text("hold SPASE key", width/2, height/3);
 	text("today's dinner is...", width/2, 3*height/7);
 	if(keyPressed && key==' '){
@@ -41,9 +60,28 @@ void draw() {
 	text(pop, width/2, 3*height/4);
 	if(roll<0)text("one more?", width/2, 5*height/6);
 }
+if(mode==1){
+	background(128,128,250);
+
+	text("hold SPASE key", width/2, height/3);
+	text("today's dinner is...", width/2, 3*height/7);
+	if(keyPressed && key==' '){
+		i=int(random(0,cook.size()-1));
+		pop =  cook.get(i);
+		println(pop);
+	}else if(keyPressed && key!=' '){
+	text("that is "+key+" key", width/2, 5*height/6);}
+	text(pop, width/2, 3*height/4);
+	if(roll<0)text("one more?", width/2, 5*height/6);
+}
+}
 
 void keyPressed() {// while spase key pressed rikk slot
 	roll=1; //rolling now
+	if(command==0&&key=='c')command+=1;else command=0;
+	if(command==1&&key=='o')command+=1;else command=0;
+	if(command==2&&key=='0')command+=1;else command=0;
+	if(command==0&&key=='k'){println("mode: "+mode);mode=1;command=0;}else command=0;
 }
 
 void keyReleased(){
